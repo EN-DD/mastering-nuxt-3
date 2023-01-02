33070e7d845cd2fce72a366b1cae7f06f38d36de
Ed Noel <ed@endd.co.uk>
2023-01-02 16:08:36 +0100

    chapter 4

---
+++ error.vue
@@ -0,0 +1,28 @@
+<template>
+  <NuxtLayout>
+    <div>
+      <h1>Error</h1>
+      <p>Something went wrong</p>
+      <p>
+        <strong>{{ error.message }}</strong>
+      </p>
+      <div v-if="error.statusCode == '404'">
+        <h1>404 - page does not exist</h1>
+      </div>
+    </div>
+    <p>
+      Go to the
+      <a class="hover:cursor-pointer" @click="handleError">first lesson</a>
+    </p>
+  </NuxtLayout>
+</template>
+
+<script setup>
+const error = useError();
+const handleError = () => {
+  clearError({
+    redirect:
+      "/course/chapter/1-chapter-1/lesson/1-introduction-to-typescript-with-vue-js-3",
+  });
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,11 @@
-<template>Hello, world!</template>
+<template>
+  <NuxtLink class="underline font-medium" :href="firstLesson.path">
+    Go to first lesson
+  </NuxtLink>
+</template>
+<script setup>
+const firstLesson = useFirstLesson();
+definePageMeta({
+  layout: false,
+});
+</script>
